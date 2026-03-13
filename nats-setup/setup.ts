2c28ccd354db6a03cff55c0c9a3e8a3c71d3f300
Naseem Siddqui | MAQ Software <naseems@maqsoftware.com>
2026-03-13 12:28:40 +0530

    nats setup updated

--- nats-setup/setup.ts
+++ nats-setup/setup.ts
@@ -7,16 +7,25 @@ async function init(){
     const nc = await connect({servers:"nats://nats-srv:4222"});
     const jsm = await nc.jetstreamManager();
 
-    // create stream (persistance storage) for all event related to post
+    // streams (persistance storage) 
     await jsm.streams.add({
         name:Streams.Post,
         subjects:["post.*"]
     })
-    // create consumer for stream POST
-    await jsm.consumers.add("POST",{
+    await jsm.streams.add({
+        name: Streams.Comment,
+        subjects:["comment.*"]
+    })
+    //  consumers 
+    await jsm.consumers.add(Streams.Post,{
         durable_name:Consumers.PostService,
         ack_policy:AckPolicy.Explicit,
         filter_subject:"post.*"
+    })
+    await jsm.consumers.add(Streams.Comment,{
+        durable_name: Consumers.CommentService,
+        ack_policy:AckPolicy.Explicit,
+        filter_subject:"comment.*"
     })
     console.log("stream and consumer created !!");
     nc.close();
